6378fadc2e956be9fc1a06cdf5bee1ea498921e0
Taylor Peat <taylorpeat@hotmail.com>
2017-07-25 17:09:09 -0400

    row comparison

--- queries.sql
+++ queries.sql
@@ -14,3 +14,7 @@ SELECT name AS "Highest Bid Less Than 100 Dollars" FROM items
 WHERE 100 > ALL (SELECT amount FROM bids WHERE item_id = items.id);
 
 SELECT max(SELECT COUNT(*) FROM bids GROUP BY bidder_id);
+
+SELECT name, count(SELECT * FROM bids b WHERE i.id = b.item_id) FROM items i;
+
+SELECT id FROM items WHERE ('Painting', 100.00, 250.00) = (SELECT name, initial_price, sales_price);
