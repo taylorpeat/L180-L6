469426825f5092245f8cc97031cf304fa6f4fa3c
Taylor Peat <taylorpeat@hotmail.com>
2017-07-25 15:16:00 -0400

    Setup schema

---
+++ schema.sql
@@ -0,0 +1,18 @@
+CREATE TABLE bidders (
+  id serial PRIMARY KEY,
+  name text NOT NULL
+);
+
+CREATE TABLE items (
+  id serial PRIMARY KEY,
+  name text NOT NULL,
+  initial_price numeric(6,2) NOT NULL CHECK(initial_price > 0),
+  sales_price numeric(6,2) CHECK(sales_price >= 0)
+);
+
+CREATE TABLE bids (
+  id serial PRIMARY KEY,
+  bidder_id integer NOT NULL REFERENCES bidders(id) ON DELETE CASCADE,
+  item_id integer NOT NULL REFERENCES items(id) ON DELETE CASCADE,
+  amount numeric(6,2) NOT NULL CHECK(amount >= 0)
+);
